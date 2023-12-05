1893164d35fa956554b47d4e386e5a0d922cf962
Minh Tho DUONG <s223749059@deakin.edu.au>
2023-12-06 10:35:28 +1100

    refactor: create initial files for migration of task-status-selector

---
+++ src/app/tasks/task-status-selector/task-status-selector.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, Inject } from '@angular/core';
+
+@Component({
+  selector: 'task-status-selector',
+  templateUrl: 'task-status-selector.component.html',
+  styleUrls: ['task-status-selector.component.scss'],
+})
+export class TaskStatusSelectorComponent {
+  constructor() {}
+}
